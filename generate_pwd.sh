c735db9dff90922fc46a2e20ed29f132b45e3ed5
Vadim <vadopolski@gmail.com>
2021-03-29 19:33:28 +0300

    test

--- generate_pwd.sh
+++ generate_pwd.sh
@@ -1,6 +1,52 @@
 students=(
-  student65555_2
-  student1121
+iavseenko
+iakimenko
+dvorobyov
+ygladkikh
+kgrishchenko
+yderkachev
+adubinsky
+adyakov
+aermokhin
+vzharkov
+izykov
+oilyin
+mkachaev
+kalexander
+dkoneva
+kdenis
+akorotkikh
+alapenko
+alevanov
+vlomakov
+amakarov
+nmezhevova
+mdmitry
+nnaranov
+nevgeny
+gosipenkov
+kpakhtusov
+sperfilyev
+pdmitry
+araev
+pryzhov
+asemyonov
+dtirskikh
+etishchenko
+aurzov
+yfurman
+acheryabkin
+ashanin
+ashpynov
+bektova
+eyarov
+ayashin
+dshubina
+dlybin
+dsinelnikov
+msipatov
+asamoilov
+dutkina
       )
       for student in "${students[@]}"; do
               p=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w16 | head -n1)
