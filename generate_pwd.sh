de0f405c2a17863e5b66f2c040afa854a4d490c7
Vadim <vadopolski@gmail.com>
2021-03-30 23:31:51 +0300

    new user

--- generate_pwd.sh
+++ generate_pwd.sh
@@ -1,53 +1,5 @@
 students=(
-iavseenko
-iakimenko
-dvorobyov
-ygladkikh
-kgrishchenko
-yderkachev
-adubinsky
-adyakov
-aermokhin
-vzharkov
-izykov
-oilyin
-mkachaev
-kalexander
-dkoneva
-kdenis
-akorotkikh
-alapenko
-alevanov
-vlomakov
-amakarov
-nmezhevova
-mdmitry
-nnaranov
-nevgeny
-gosipenkov
-kpakhtusov
-sperfilyev
-pdmitry
-araev
-pryzhov
-asemyonov
-dtirskikh
-etishchenko
-aurzov
-yfurman
-acheryabkin
-ashanin
-ashpynov
-bektova
-eyarov
-ayashin
-dshubina
-dlybin
-dsinelnikov
-msipatov
-asamoilov
-dutkina
-      )
+egorios)
       for student in "${students[@]}"; do
               p=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w16 | head -n1)
               useradd -m $student
